ecd5fddbfe9a617793f4cafde0fbb6048e9d08cb
ZinchJul <150132146+ZinchJul@users.noreply.github.com>
2025-01-14 16:56:50 +0400

    Add files via upload

---
+++ models.py
@@ -0,0 +1,73 @@
+from datetime import datetime
+from typing import Dict, Any
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import create_engine, Integer, String, ForeignKey, UniqueConstraint, DateTime, func
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import DeclarativeBase
+
+
+class Base(DeclarativeBase):
+  pass
+
+db = SQLAlchemy(model_class=Base)
+
+
+class Client(db.Model):
+    __tablename__ = 'client'
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(50))
+    surname: Mapped[str] = mapped_column(String(50))
+    credit_card: Mapped[str] = mapped_column(String(50), nullable=True)
+    car_number: Mapped[str] = mapped_column(String(10), nullable=True)
+
+    def __repr__(self):
+        return f'Client {self.name} {self.surname}, credit card - {self.credit_card}, car number {self.car_number}'
+
+    def to_json(self) -> Dict[str, Any]:
+        return {c.name: getattr(self, c.name) for c in
+                self.__table__.columns}
+
+
+class Parking(db.Model):
+    __tablename__ = 'parking'
+    id: Mapped[int] = mapped_column(primary_key=True)
+    address: Mapped[str] = mapped_column(String(100))
+    opened: Mapped[bool]
+    count_places: Mapped[int]
+    count_available_places: Mapped[int]
+
+    def __repr__(self):
+        return f'Parking address {self.address}, opened {self.opened}, count places {self.count_places}'
+
+    def to_json(self) -> Dict[str, Any]:
+        return {c.name: getattr(self, c.name) for c in
+                self.__table__.columns}
+
+
+class Client_Parking(db.Model):
+    __tablename__ = 'client_parking'
+    id: Mapped[int] = mapped_column(primary_key=True)
+    client_id: Mapped[int] = mapped_column(ForeignKey('client.id'))
+    parking_id: Mapped[int] = mapped_column(ForeignKey('parking.id'))
+    time_in: Mapped[datetime] = mapped_column(server_default=func.now(), nullable=True)
+    time_out: Mapped[datetime] = mapped_column(server_default=func.now(), nullable=True)
+
+    UniqueConstraint('client_id', 'parking_id', name='unique_client_parking')
+    client = db.relationship('Client', backref='client_parking')
+    parking = db.relationship('Parking', backref='client_parking')
+
+    def __repr__(self):
+        return f'Client {self.client_id} in parking {self.parking_id} from {self.time_in} until {self.time_out}'
+
+    def to_json(self) -> Dict[str, Any]:
+        json_data = {
+            c.name: getattr(self, c.name)
+            for c in self.__table__.columns
+        }
+        json_data['parking'] = self.parking.to_json()
+        json_data['client'] = self.client.to_json()
+        return json_data
+
+
+
