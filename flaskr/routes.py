493db152e51781c7e7f53b77a1bbc20214b038eb
ZinchJul <150132146+ZinchJul@users.noreply.github.com>
2025-01-15 12:49:33 +0400

    Update routes.py

--- flaskr/routes.py
+++ flaskr/routes.py
@@ -1,6 +1,6 @@
 from flask import request, jsonify
 from datetime import datetime
-from module_29_testing.hw.models import db, Client, Client_Parking, Parking
+from models import db, Client, Client_Parking, Parking
 
 
 
