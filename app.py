61e791cf45bcb1df5d323d5553285665eab244c1
ZinchJul <zinchenkoy@mail.ru>
2025-01-16 19:47:16 +0400

    changes with black and isort

--- app.py
+++ app.py
@@ -1,22 +1,42 @@
 from flask import Flask
-from models import Client, Parking, db
 from flaskr.routes import register_routes
 
+from models import Client, Parking, db
 
 data_add_clients = (
-    Client(name='Ivan', surname='Petrov', credit_card='Sber 2200 ** 4455', car_number='o 654 кп 63 rus'),
-    Client(name='Svetlana', surname='Sergeeva', credit_card='VTB 2200 ** 1234', car_number='у 574 лд 74 rus')
+    Client(
+        name="Ivan",
+        surname="Petrov",
+        credit_card="Sber 2200 ** 4455",
+        car_number="o 654 кп 63 rus",
+    ),
+    Client(
+        name="Svetlana",
+        surname="Sergeeva",
+        credit_card="VTB 2200 ** 1234",
+        car_number="у 574 лд 74 rus",
+    ),
 )
 
 data_add_parkings = (
-    Parking(address='Самара, ул.Парковая, 1д', opened=True, count_places=50, count_available_places=50),
-    Parking(address='Самара, ул.Съездовская, 15', opened=True, count_places=150, count_available_places=150)
+    Parking(
+        address="Самара, ул.Парковая, 1д",
+        opened=True,
+        count_places=50,
+        count_available_places=50,
+    ),
+    Parking(
+        address="Самара, ул.Съездовская, 15",
+        opened=True,
+        count_places=150,
+        count_available_places=150,
+    ),
 )
 
 
 def create_app():
     app = Flask(__name__)
-    app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///parking_free.db'
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///parking_free.db"
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.init_app(app)
 
@@ -37,7 +57,6 @@ def create_app():
     return app
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app = create_app()
     app.run()
-
